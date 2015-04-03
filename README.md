ef2757cc5519b2b706ac56239438b7d25ea33eab
Christian Lohr <christian.lohr@gmail.com>
2015-04-03 10:43:30 -0400

    Added iron for encryption

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# hapi-yar
-Basic hapi reverse proxy setup with yar
+# hapi-auth
+Authentication proof of concept using [hapi](https://github.com/hapijs) as a reverse proxy with [iron](https://github.com/hueniverse/iron).
 
-![Hapi-yar](image/pirate.jpg)
+![Hapi-auth](image/glove.jpeg)
 
 ## Auth Flow
-* User visits site for first time, no cookie, generate token payload, serialize, then store in an HTTP only cookie
-* User visits site with expired cookie, generate a new token payload, serialize, then and store in an HTTP only cookie
+* User visits site for first time, no cookie, generate token payload, encrypt, then store in an HTTP only cookie
+* User visits site with expired cookie, generate a new token payload, encrypt, then and store in an HTTP only cookie
 * User visits site with invalid token TOKEN_FRESHNESS threshold but has not expired, pro-actively re-auth the user
-* User visits site with invalid token TOKEN_FRESHNESS threshold and is expired, generate a new token payload, serialize, then and store in an HTTP only cookie
+* User visits site with invalid token TOKEN_FRESHNESS threshold and is expired, generate a new token payload, encrypt, then and store in an HTTP only cookie
--- plugins/token.js
+++ plugins/token.js
@@ -2,21 +2,31 @@
 
 var AUTH_PAYLOAD = require('../lib/contstants').AUTH_PAYLOAD;
 var TOKEN_FRESHNESS = require('../lib/contstants').TOKEN_FRESHNESS; // minutes
+var IRON_PASSWORD = require('../lib/contstants').IRON_PASSWORD;
 var tokenService = require('../lib/tokenService');
-var serialize = require('../lib/utils').serialize;
-var parse = require('../lib/utils').parse;
+var Hoek = require('hoek');
 var R = require('ramda');
+var Iron = require('iron');
 
 var internals = {
     setUpAuthCookie: function(reply) {
         var payload = tokenService.getAndSetToken(null);
         var fullPayload = R.compose(R.assoc('createdAt', Date.now()))(payload);
-        reply.state(AUTH_PAYLOAD, serialize(fullPayload));
+        return Iron.seal(fullPayload, IRON_PASSWORD, Iron.defaults, function (err, sealedObj) {
+            Hoek.assert(!err, err);
+            reply.state(AUTH_PAYLOAD, sealedObj);
+            return reply.continue();
+        });
     },
     reAuthCookie: function (reply, token) {
         var payload = tokenService.reAuthenticateToken(token);
         payload.createdAt = Date.now();
-        reply.state(AUTH_PAYLOAD, serialize(payload));
+        console.log('reAuthing Token:', token);
+        return Iron.seal(payload, IRON_PASSWORD, Iron.defaults, function (err, sealedObj) {
+            Hoek.assert(!err, err);
+            reply.state(AUTH_PAYLOAD, sealedObj);
+            return reply.continue();
+        });
     },
     msToMinutes: R.divide(R.__, 60000)
 };
@@ -24,22 +34,21 @@ var internals = {
 exports.register = function(server, options, next) {
     server.ext('onPreAuth', function(request, reply) {
         var token = request && request.state[AUTH_PAYLOAD];
-        var parsedToken, tokenLife;
-
         if (!token) {
             // token does not exist, get a token before continuing action
-            internals.setUpAuthCookie(reply);
+            return internals.setUpAuthCookie(reply);
         }
-
-        parsedToken = parse(token);
-        tokenLife = internals.msToMinutes(Date.now() - parsedToken.createdAt);
-        if (tokenLife > TOKEN_FRESHNESS) {
-            // token exists and needs to be pro-actively re-authed before continuing action
-            internals.reAuthCookie(reply, token);
-        }
-
-        // token exists and is valid and within freshness limit, continue action
-        return reply.continue();
+        return Iron.unseal(token, IRON_PASSWORD, Iron.defaults, function (err, unsealed) {
+            var tokenLife;
+            Hoek.assert(!err, err);
+            tokenLife = internals.msToMinutes(Date.now() - unsealed.createdAt);
+            if (tokenLife > TOKEN_FRESHNESS) {
+                // token exists and needs to be pro-actively re-authed before continuing action
+                return internals.reAuthCookie(reply, unsealed.auth_token);
+            }
+            // token exists and is valid and within freshness limit, continue action
+            return reply.continue();
+        });
     });
 
     return next();
