5d9d6d910f1e8c1b2332555715762b3acbe8df59
Christian Lohr <christian.lohr@gmail.com>
2015-04-03 11:05:20 -0400

    Added console.logs to validate POC auth flow

--- plugins/token.js
+++ plugins/token.js
@@ -12,19 +12,22 @@ var internals = {
     setUpAuthCookie: function(reply) {
         var payload = tokenService.getAndSetToken(null);
         var fullPayload = R.compose(R.assoc('createdAt', Date.now()))(payload);
+        console.log('setUpAuthCookie:', payload);
         return Iron.seal(fullPayload, IRON_PASSWORD, Iron.defaults, function (err, sealedObj) {
             Hoek.assert(!err, err);
             reply.state(AUTH_PAYLOAD, sealedObj);
+            console.log('seal auth token:', sealedObj);
             return reply.continue();
         });
     },
     reAuthCookie: function (reply, token) {
         var payload = tokenService.reAuthenticateToken(token);
         payload.createdAt = Date.now();
-        console.log('reAuthing Token:', token);
+        console.log('reAuthCookie:', payload);
         return Iron.seal(payload, IRON_PASSWORD, Iron.defaults, function (err, sealedObj) {
             Hoek.assert(!err, err);
             reply.state(AUTH_PAYLOAD, sealedObj);
+            console.log('seal re-auth token:', sealedObj);
             return reply.continue();
         });
     },
@@ -36,14 +39,17 @@ exports.register = function(server, options, next) {
         var token = request && request.state[AUTH_PAYLOAD];
         if (!token) {
             // token does not exist, get a token before continuing action
+            console.log('token does not exist, get a token before continuing action');
             return internals.setUpAuthCookie(reply);
         }
+
         return Iron.unseal(token, IRON_PASSWORD, Iron.defaults, function (err, unsealed) {
             var tokenLife;
             Hoek.assert(!err, err);
             tokenLife = internals.msToMinutes(Date.now() - unsealed.createdAt);
             if (tokenLife > TOKEN_FRESHNESS) {
                 // token exists and needs to be pro-actively re-authed before continuing action
+                console.log('token exists and needs to be pro-actively re-authed before continuing action');
                 return internals.reAuthCookie(reply, unsealed.auth_token);
             }
             // token exists and is valid and within freshness limit, continue action
